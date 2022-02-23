a1295cec9d53789b006458f035251c3d310e7e57
johnmartins <johnmartins1992@gmail.com>
2022-02-23 10:00:15 +0100

    Removed log from filter instantiation

--- src/models/plots/DataFilter.js
+++ src/models/plots/DataFilter.js
@@ -3,7 +3,6 @@ class DataFilter {
     static counter = 0;
 
     constructor (property, thresholdA, thresholdB) {
-        console.warn("NEW FILTER INSTANTIATED")
         this.property = property    // Category name
         this.thresholdA = thresholdA        
         this.thresholdB = thresholdB      
