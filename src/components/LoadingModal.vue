6f998fd3b4c92e71781f9560c391097f4b23743b
johnmartins <johnmartins1992@gmail.com>
2023-02-14 14:50:39 +0100

    Staged new files

---
+++ src/components/LoadingModal.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="modal-mask modal-container" v-if="loadingReason">
+        <div class="message-container">
+            <span>{{loadingReason}}</span><br />
+            <span class="icon"><font-awesome-icon icon="fa-solid fa-spinner" class="anim-rotate-step-8" /></span>
+            
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { storeToRefs } from "pinia"
+
+import {useStateStore} from "@/store/StateStore"
+
+const stateStore = useStateStore()
+const {loadingReason} = storeToRefs(stateStore)
+
+</script>
+
+<style lang="scss" scoped>
+    $height: 80px;
+    $width: 300px;
+
+    .modal-container {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center; /*x-axis*/
+        align-items: center; /*y-axis*/
+    }
+
+    .message-container {
+        color: white;
+    }
+</style>
---
+++ src/fontawesome.js
@@ -0,0 +1,7 @@
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTrash, faCirclePlus, faRightLeft, faBars, faPenToSquare, faBroom, faSpinner } from "@fortawesome/free-solid-svg-icons"
+
+library.add(faTrash, faCirclePlus, faRightLeft, faBars, faPenToSquare, faBroom, faSpinner)
+
+export default FontAwesomeIcon
