3c69fc2b02bfa56d50aba210de96bcf3807262c6
johnmartins <johnmartins1992@gmail.com>
2023-04-20 11:42:58 +0200

    sub-dir build option

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
     productionSourceMap: false,
-    
+    publicPath: process.env.VUE_APP_SUBDIR ? VUE_APP_SUBDIR : '',
 }
