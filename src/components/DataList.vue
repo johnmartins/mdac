5245fcb7c91d53dbf7b5484e46961b13a921f2b6
johnmartins <johnmartins1992@gmail.com>
2023-05-09 13:51:20 +0200

    CSS overhaul of data view

--- src/components/DataList.vue
+++ src/components/DataList.vue
@@ -1,22 +1,29 @@
 <template>
-    <div style="overflow: auto; height: 100%;">
-        <div class="table-container">
-            <table class="table table-sm table-striped">
-                <thead>
-                    <tr>
-                        <th v-for="c in categories" :key="c.id">
-                            <div><span>{{c.title}}</span></div>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody class="table-bordered">
-                    <tr v-for="(d, index) in data.filter(dataStore.dataPointFilterCheck)" :key="index">
-                        <td v-for="c in categories" :key="c.id">
-                            {{d[c.title]}}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+    <div class="container-fuild" style="height: 100%; overflow: auto;">
+        <div class="row p-0 m-0">
+            <div class="col p-0 m-0">
+                <!-- Future data list tools goes here -->
+            </div>
+        </div>
+        <div class="row p-0 m-0">
+            <div class="col p-0 m-0">
+                <table class="table-fixed-header table table-sm table-striped">
+                    <thead>
+                        <tr>
+                            <th class="header" v-for="c in categories" :key="c.id">
+                                <div class="rotated-header"><span>{{ c.title }}</span></div>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-bordered">
+                        <tr v-for="(d, index) in data.filter(dataStore.dataPointFilterCheck)" :key="index">
+                            <td v-for="c in categories" :key="c.id">
+                                {{ d[c.title] }}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </template>
@@ -34,23 +41,34 @@ const {data, filters, categories} = storeToRefs(dataStore)
 </script>
 
 <style lang="scss" scoped>
-
-    .table-container {
-        padding: 8px;
-    }
-
-    .table {
+    .table-fixed-header {
         font-size: 0.7em;
-        border-spacing: collapse;
+        font-family: monospace;
+        overflow: auto;
+        border-collapse: separate;
+        border-spacing: 0;
+
+        .header {
+            vertical-align: bottom;
+        }
+
+        thead tr {
+            position: sticky;
+            top: 0;
+            z-index: 1;
+            background: white;
+        }
 
         th {
             height: 140px;
             white-space: nowrap;
             padding: 0 !important;
+            border-bottom: 2px solid black;
+            
         }
 
-        th > div {
-            transform: translate(13px, 0) rotate(310deg);
+        .rotated-header {
+            transform: translate(0.2em, 0) rotate(310deg);
             width: 30px;
         }
 
