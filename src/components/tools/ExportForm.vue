aa7ab8e9dc894c155d1a72eea5e1ced703e589b5
johnmartins <johnmartins1992@gmail.com>
2023-07-26 11:57:07 +0200

    Fully functional CSV export

--- src/components/tools/ExportForm.vue
+++ src/components/tools/ExportForm.vue
@@ -21,7 +21,7 @@
                 <input ref="filteredDataCheckbox" v-model="includeFilteredData" type="checkbox">
             </div>
             <div class="d-grid gap-2 mt-2">
-                <button class="btn btn-sm btn-primary" @click="exportDataRequest">Download CSV</button>
+                <button class="btn btn-sm btn-primary" @click="exportDataRequest(includeFilteredData)">Download CSV</button>
             </div>
         </div>
     </div>
@@ -47,13 +47,56 @@ function exportRequest (evt) {
     eventBus.emit('ExportForm.exportFigureRequest', formatSelector.value.value)
 }
 
-function exportDataRequest (evt) {
-    // Fetch data
-    console.log(dataStore.data)
+function exportDataRequest (includeFiltered) {
+    // Get data
+    let data = []
+    if (includeFiltered) {
+        data = dataStore.data
+    } else {
+        data = dataStore.data.filter(dataStore.dataPointFilterCheck)
+    }
 
     // Parse and convert to CSV
+    let csvRowArray = []
 
+    // Add header
+    let headerStrArray = []
+    for (let c of dataStore.categories) {
+        headerStrArray.push(c.displayTitle)
+    }
+    csvRowArray.push(headerStrArray.join(";"))
 
+    // Add data
+    for (let d of data) {
+        let valueStrArray = []
+
+        for (let c of dataStore.categories) {   // TODO: This is slow
+            let valueStr = ""
+            let value = d[c.title]
+
+            if (!value) { 
+                valueStrArray.push(valueStr) 
+                continue
+            }
+            if (c.usesCategoricalData) {
+                valueStrArray.push("\"" + String(value) + "\"")
+                continue
+            }
+
+            valueStrArray.push(value)
+        }
+
+        csvRowArray.push(valueStrArray.join(";"))
+    }
+
+    const csvStr = csvRowArray.join("\n")
+    
+    // Download csvStr as csv-file
+    const blob = new Blob([csvStr], { type: 'text/csv;charset=utf-8;' })
+    const a = document.createElement("a")
+    a.href = URL.createObjectURL(blob);
+    a.download = 'mdac-data.json';
+    a.click();
 }
 
 </script>
