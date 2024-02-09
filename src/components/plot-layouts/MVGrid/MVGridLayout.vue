9ac0e752febfe660307252099b358adbd916100e
Julian Martinsson Bonde <johnmartins1992@gmail.com>
2024-02-09 15:21:07 +0100

    Bumped version to 1.13.1
    * Enabled jumping from scatter grid to scatter plot
    
    * Bumped version to 1.13.1

--- src/components/plot-layouts/MVGrid/MVGridLayout.vue
+++ src/components/plot-layouts/MVGrid/MVGridLayout.vue
@@ -3,16 +3,31 @@
         <div class="mv-grid-container">
             <div class="grid-row">
                 <div class="grid-element grid-title-element"></div>
-                <div class="grid-element" style="display: flex; justify-content: center; align-items: center;" v-for="cxTitleCat, xIndex in categories" :key="cxTitleCat.title" :title="cxTitleCat.title" >
+                <div 
+                    class="grid-element" 
+                    style="display: flex; justify-content: center; align-items: center;" 
+                    v-for="cxTitleCat, xIndex in categories.filter(cat => cat.enabled)" 
+                    :key="cxTitleCat.title" 
+                    :title="cxTitleCat.title" 
+                >
                     {{ getCatIdentifier(xIndex) }}
                 </div>
             </div>
-            <div class="grid-row" v-for="cy, yIndex in categories" :key="cy.title">
-                <div class="grid-element grid-title-element" style="display: flex; align-items: center;" :title="cy.title">
+            <div 
+                class="grid-row" 
+                v-for="cy, yIndex in categories.filter(cat => cat.enabled)" 
+                :key="cy.title"
+            >
+                <div 
+                    class="grid-element grid-title-element" 
+                    style="display: flex; align-items: center;" 
+                    :title="cy.title"
+                >
                     {{ `${getCatIdentifier(yIndex)}: ${cy.title}` }}
                 </div>
+
                 <div class="grid-element" 
-                    v-for="cx in categories" 
+                    v-for="cx in categories.filter(cat => cat.enabled)" 
                     :key="cx.title" 
                     :title="`X: ${cx.title}, Y: ${cy.title}`" 
                 >
@@ -22,6 +37,7 @@
                         :dataArray="includedData"
                         :renderCondition="checkViewActive"
                         ref="rasterElementArray"
+                        @click="makePlot(cx, cy)"
                     />
                 </div>
             </div>
@@ -35,14 +51,20 @@ import { nextTick, ref, watch, inject } from 'vue';
 import { useDataStore } from '@/store/DataStore';
 import { useStateStore } from '@/store/StateStore';
 import { useOptionsStore } from '@/store/OptionsStore';
+import { useScatterStore } from '@/store/ScatterStore';
 import { storeToRefs } from 'pinia';
 import Filter from '@/models/filters/Filter';
 
 import ScatterPlotPointLayerRaster from '../ScatterPlot/ScatterPlotPointLayerRaster.vue';
 
+// Models
+import ScatterPlotConfig from '@/models/plots/ScatterPlotConfig';
+
+// Stores
 const dataStore = useDataStore();
 const stateStore = useStateStore();
 const optionsStore = useOptionsStore();
+const scatterStore = useScatterStore();
 
 const { categories, data } = storeToRefs(dataStore);
 const { activeView, reRenderMvGrid } = storeToRefs(stateStore);
@@ -121,12 +143,35 @@ function getCatIdentifier (index) {
     }
 }
 
+async function makePlot (cx, cy) {
+    console.log("Make plot", cx, cy);
+
+    const newPlot = new ScatterPlotConfig();
+    scatterStore.addPlot(newPlot);
+
+    newPlot.xAxisCategoryName = cx.title;
+    newPlot.yAxisCategoryName = cy.title;
+    newPlot.title = `${cx.displayTitle} vs. ${cy.displayTitle}`;
+
+
+    // Update selected plot to new plot
+    scatterStore.setSelectedPlot(newPlot.id);
+
+    await nextTick();
+
+    eventBus.emit('Router.RequestTabChange', 'scatter');
+}
+
 </script>
 
 <style lang="scss" scoped>
 
+@import '@/scss/Colors.scss';
+
 .mv-grid-container {
     color: black;
+    font-family: monospace;
+    font-size: 0.8em;
     max-width: calc(100vh + 40px);
     overflow: hidden;
     padding: 16px;
@@ -146,10 +191,15 @@ function getCatIdentifier (index) {
         font-size: 1em;
         white-space: nowrap;
         overflow: hidden;
+        cursor: pointer;
     
         &.grid-title-element {
             aspect-ratio: unset;
             grid-column: 1 / span 4;
+        }
+
+        &:hover {
+            filter: invert(100%);
         }
     }
 }
