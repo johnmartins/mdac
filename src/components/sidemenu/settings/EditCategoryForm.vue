667076f7af37ad40525809e601f49399df6c566c
Julian Martinsson Bonde <julianm@chalmers.se>
2024-08-20 08:40:14 +0200

    Fixed bug that caused raster PCP not to redraw on axis change

--- src/components/sidemenu/settings/EditCategoryForm.vue
+++ src/components/sidemenu/settings/EditCategoryForm.vue
@@ -28,55 +28,59 @@
 </template>
 
 <script setup>
-import { ref, watch } from "vue"
-import { storeToRefs } from "pinia"
+import { ref, watch, inject } from "vue";
+import { storeToRefs } from "pinia";
 
 // Stores
-import {useDataStore} from "@/store/DataStore"
-import {useStateStore} from "@/store/StateStore"
+import {useDataStore} from "@/store/DataStore";
+import {useStateStore} from "@/store/StateStore";
 
 // Components
-import TextInput from '@/components/inputs/TextInput'
-import SidebarSection from '@/components/layouts/SidebarSection'
+import TextInput from '@/components/inputs/TextInput';
+import SidebarSection from '@/components/layouts/SidebarSection';
     
 // Models
-import Category from '@/models/plots/Category'
+import Category from '@/models/plots/Category';
 
 // DOM
-const sidebarSection = ref(null)
+const sidebarSection = ref(null);
 
-const dataStore = useDataStore()
-const stateStore = useStateStore()
+const dataStore = useDataStore();
+const stateStore = useStateStore();
 
-const {selectedCategory} = storeToRefs(stateStore)
-const editedCategory = ref(null)
+const {selectedCategory} = storeToRefs(stateStore);
+const editedCategory = ref(null);
+
+const eventBus = inject('eventBus');
 
 watch(selectedCategory, () => {
     if (!selectedCategory.value) {
-        editedCategory.value = null
-        return
+        editedCategory.value = null;
+        return;
     }
 
-    sidebarSection.value.maximized = true
+    sidebarSection.value.maximized = true;
 
     // The reason we do this insead of using v-models directly on the inputs is because
     // utilizing v-models for huge data sets can be very slow. Decoupling the edited and real variable
     // allows the user to edit the category with normal speed regardless of data set size.
-    let newCat = new Category("$$TEMPORARY_CATEGORY$$", 0, 1, {overwrite: true})
-    newCat.morph(selectedCategory.value, {migrateReference: false})
-    editedCategory.value = newCat
+    let newCat = new Category("$$TEMPORARY_CATEGORY$$", 0, 1, {overwrite: true});
+    newCat.morph(selectedCategory.value, {migrateReference: false});
+    editedCategory.value = newCat;
 })
 
 function disableCategory () {
-    selectedCategory.value.enabled = false
+    selectedCategory.value.enabled = false;
 }
 
 function enableCategory () {
-    selectedCategory.value.enabled = true
+    selectedCategory.value.enabled = true;
 }
 
 function editCategory () {
-    selectedCategory.value.morph(editedCategory.value, {migrateReference: false})
+    selectedCategory.value.morph(editedCategory.value, {migrateReference: false});
+    // Request redraw
+    eventBus.emit('RequestPCPRedraw');
 }
 
 </script>
