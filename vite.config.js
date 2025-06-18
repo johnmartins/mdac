96fbc057d4716bbe3fd3af5a133f6016629e945f
Julian Martinsson Bonde <johnmartins1992@gmail.com>
2025-06-18 23:39:35 +0200

    Instructions for local execution

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 import packageJSON from './package.json';
 
@@ -10,6 +11,7 @@ export default defineConfig({
     plugins: [
         vue(),
         vueDevTools(),
+        // viteSingleFile() // Uncomment this for local build.
     ],
     define: {
         'import.meta.env.VITE_APP_VERSION': JSON.stringify(packageJSON.version),
